ba2a80718397e9f510fe74bdc09a4654c76c0471
Gogulkannan <gkanna005@gmail.com>
2017-01-31 13:32:12 +0800

    gokul pushing-6

--- app/src/main/java/com/example/gogul/adandroid/DisbursementMainmenu.java
+++ app/src/main/java/com/example/gogul/adandroid/DisbursementMainmenu.java
@@ -191,7 +191,7 @@ public class DisbursementMainmenu extends AppCompatActivity
         final Dialog d = new Dialog(this);
         d.setTitle(deptname);
         d.setContentView(R.layout.successchange);
-        d.setCancelable(true);
+        d.setCancelable(false);
         TextView msgread= (TextView)d.findViewById(R.id.messagedia);
         msgread.setText("have changed the collection point to "+location+".");
         Button t = (Button) d.findViewById(R.id.btnsuccok);
